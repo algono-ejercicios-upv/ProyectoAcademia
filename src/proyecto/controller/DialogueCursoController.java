b06ef1a18647192c2261fe01d88c5bede0de31a6
Alejandro Gómez <algono@inf.upv.es>
2018-04-04 22:04:19 +0200

    Creado control de errores para la creación de cursos.

--- src/proyecto/controller/DialogueCursoController.java
+++ src/proyecto/controller/DialogueCursoController.java
@@ -38,7 +38,7 @@ import javafx.util.converter.IntegerStringConverter;
  * @author Alejandro
  */
 public class DialogueCursoController implements Initializable {
-    
+
     @FXML
     private Button buttonCreate;
     @FXML
@@ -63,10 +63,12 @@ public class DialogueCursoController implements Initializable {
     private TextField textAula;
     @FXML
     private ToolBar toolBarDias;
-    
+
     private Stage primaryStage;
+    public static final Dias[] SEMANA = Dias.values();
     private boolean errorSpinner = false;
-    
+    public static Alert errorAlert = new Alert(AlertType.ERROR);
+
     class CyclicIntegerSpinnerValueFactory extends IntegerSpinnerValueFactory {
         public CyclicIntegerSpinnerValueFactory(int min, int max) {
             super(min, max);
@@ -91,16 +93,16 @@ public class DialogueCursoController implements Initializable {
         @Override
         public void decrement(int steps) {
             if (getValue() == null) { errorSpinner = true; setValue(0); }
-            else if (getValue() == getMin()) { setValue(getMax()); super.decrement(steps-1); } 
+            else if (getValue() == getMin()) { setValue(getMax()); super.decrement(steps-1); }
             else { super.decrement(steps); }
         }
     }
-    
+
     public void init(Stage stage, Curso curso) {
         primaryStage = stage;
         stage.setTitle("Nuevo Curso");
         if (curso != null) {
-            textTitulo.setText(curso.getTitulodelcurso()); 
+            textTitulo.setText(curso.getTitulodelcurso());
             textProfesor.setText(curso.getProfesorAsignado());
             textAula.setText(curso.getAula());
             spinnerMaxAlumnos.getValueFactory().setValue(curso.getNumeroMaximodeAlumnos());
@@ -111,85 +113,89 @@ public class DialogueCursoController implements Initializable {
             buttonCerrar.requestFocus();
         }
     }
-    
+    /**
+     * @param dias Array ordenado (ya que {@link #getDias() getDias()} los inserta en orden).
+     */
     private void setDias(ArrayList<Dias> dias) {
-        for (Node n : toolBarDias.getItems()) {
-            ToggleButton b = ((ToggleButton) n);
-            Dias dia = null;
-            switch(b.getText().charAt(0)) { //Sabemos que el texto de los botones siempre sera el mismo
-                case 'L': dia = Dias.Lunes; break;
-                case 'M': dia = Dias.Martes; break;
-                case 'X': dia = Dias.Miercoles; break;
-                case 'J': dia = Dias.Jueves; break;
-                case 'V': dia = Dias.Viernes; break;
-                case 'S': dia = Dias.Sabado; break;
-                case 'D': dia = Dias.Domingo; break;
+        ObservableList<Node> items = toolBarDias.getItems();
+        int i = 0, j = 0;
+        while (i < dias.size()) {
+            Dias dia = dias.get(i);
+            while (j < SEMANA.length && !dia.equals(SEMANA[j])) j++;
+            if (j < SEMANA.length) {
+                ToggleButton b = (ToggleButton) items.get(j);
+                b.setSelected(true);
             }
-            b.setSelected(dias.contains(dia));
+            i++; j++;
         }
     }
-    
+    /**
+    * El orden de los botones en {@link #toolBarDias ToolBarDias} es el mismo en el que estan colocados (L,M,X,J,V,S,D)
+    * Por tanto, podemos obtener sus dias en el mismo orden
+    */
     private ArrayList<Dias> getDias() {
         ArrayList<Dias> dias = new ArrayList<>();
-        for (Node n : toolBarDias.getItems()) {
-            ToggleButton b = ((ToggleButton) n);
-            if (b.isSelected()) {
-                switch(b.getText().charAt(0)) { //Sabemos que el texto de los botones siempre sera el mismo
-                    case 'L': dias.add(Dias.Lunes); break;
-                    case 'M': dias.add(Dias.Martes); break;
-                    case 'X': dias.add(Dias.Miercoles); break;
-                    case 'J': dias.add(Dias.Jueves); break;
-                    case 'V': dias.add(Dias.Viernes); break;
-                    case 'S': dias.add(Dias.Sabado); break;
-                    case 'D': dias.add(Dias.Domingo); break;
-                }
-            }
+        ObservableList<Node> items = toolBarDias.getItems();
+        for (int i = 0; i < items.size(); i++) {
+            ToggleButton b = (ToggleButton) items.get(i);
+            if (b.isSelected()) dias.add(SEMANA[i]);
         }
         return dias;
     }
-    /** El spinner no actualiza los datos puestos a mano si el usuario no pulsa 'ENTER', 
+    /**
+     * El spinner no actualiza los datos puestos a mano si el usuario no pulsa 'ENTER',
      * asi que nos aseguramos de que lo haga antes de que se cree el curso utilizando 'spinner.increment(0)'.
+     * Ademas, comprobamos que los campos de texto no esten vacios.
      */
-    private boolean checkData() {
-        Alert errorAlert = new Alert(AlertType.ERROR);
-        boolean correct = true;
-        errorSpinner = false; //Antes de comprobar los datos, limpia el flag.
-        
-        //Comprueba que las fechas sean correctas
-        LocalDate inicio = dateInicio.getValue();
-        LocalDate fin = dateFin.getValue();
-        correct = inicio != null && fin != null && inicio.isBefore(fin);
-        if (!correct) { errorAlert.show(); return false; }
-        
+    private void checkFields() {
         ObservableList<Node> children = form.getChildren();
         int i = 0;
-        while (i < children.size() && correct) {
+        while (i < children.size()) {
             Node n = children.get(i);
-            //Comprueba que los datos de los spinners sean correctos
-            if (n instanceof Spinner) {
-                ((Spinner) n).increment(0);
-                if (errorSpinner) {
-                    errorAlert.setHeaderText("Campo erróneo");
-                    errorAlert.setContentText("Uno de los campos ha sido rellenado con un valor erróneo. Compruebe los datos y vuelva a intentarlo.");
-                    correct = false;
-                }
-            }
             //Comprueba que los campos de texto no esten vacios
-            else if (n instanceof TextField) {
+            if (n instanceof TextField) {
                 if (((TextField) n).getText().isEmpty()){
                     errorAlert.setHeaderText("Campo vacío");
-                    errorAlert.setContentText("Alguno de los campos está vacío. Rellene todos los campos y vuelva a intentarlo.");
-                    correct = false;
+                    errorAlert.setContentText("Alguno de los campos está vacío.\nRellene todos los campos y vuelva a intentarlo.");
+                    throw new InputMismatchException();
+                }
+            }
+            //Comprueba que los datos de los spinners sean correctos
+            else if (n instanceof Spinner) {
+                ((Spinner) n).increment(0);
+                if (errorSpinner) {
+                    errorAlert.setHeaderText("Número erróneo");
+                    errorAlert.setContentText("Uno de los campos númericos ha sido rellenado con un valor erróneo.\nCompruebe los datos y vuelva a intentarlo.");
+                    throw new InputMismatchException();
                 }
             }
             i++;
         }
-        if (!correct) { errorAlert.show(); return false; }
-        
-        //Si todo lo anterior fue correcto, comprueba que al menos hay un dia seleccionado
-        children = toolBarDias.getItems();
-        i = 0;
+    }
+    //Comprueba que las fechas sean correctas (Ninguna este vacia, y la de inicio sea anterior a la de fin)
+    private void checkDates() {
+        LocalDate inicio = dateInicio.getValue();
+        LocalDate fin = dateFin.getValue();
+        errorAlert.setHeaderText("Fechas incorrectas");
+        if (inicio == null || fin == null) {
+            errorAlert.setContentText("Alguna de las fechas introducidas es incorrecta.\nCompruébelas y vuelva a intentarlo.");
+            dateInicio.requestFocus();
+            throw new InputMismatchException();
+        } else if (inicio.isBefore(LocalDate.now())) {
+            errorAlert.setContentText("La fecha de inicio no debe ser anterior a la fecha de hoy.\nIntroduzca una fecha correcta y vuelva a intentarlo.");
+            dateInicio.requestFocus();
+            throw new InputMismatchException();
+        } else if (!inicio.isBefore(fin)) {
+            errorAlert.setContentText("La fecha de fin no debe ser anterior a la de inicio.\nIntroduzca una fecha correcta y vuelva a intentarlo.");
+            dateFin.requestFocus();
+            throw new InputMismatchException();
+        }
+    }
+    //Comprueba que al menos un dia haya sido seleccionado
+    private void checkDias() {
+        ObservableList<Node> children = toolBarDias.getItems();
         boolean dayPicked = false;
+        int i = 0;
         while (i < children.size() && !dayPicked) {
             ToggleButton b = (ToggleButton) children.get(i);
             dayPicked = b.isSelected();
@@ -197,18 +203,17 @@ public class DialogueCursoController implements Initializable {
         }
         if (!dayPicked) {
                 errorAlert.setHeaderText("Días no asignados");
-                errorAlert.setContentText("No ha asignado ningún día de la semana para el curso. Asigne al menos uno y vuelva a intentarlo.");
-                correct = false;
+                errorAlert.setContentText("No ha asignado ningún día de la semana para el curso.\nAsigne al menos uno y vuelva a intentarlo.");
+                throw new InputMismatchException();
         }
-        if (!correct) { errorAlert.show(); return false; }
-        
-        return correct;
     }
-    
+
     @FXML
     private void createCurso(ActionEvent event) {
-        boolean correct = checkData();
-        if (correct) {
+        try {
+            checkFields();            
+            checkDates();
+            checkDias();
             AccesoaBD acceso = new AccesoaBD();
             ArrayList<Curso> listCursos = (ArrayList<Curso>) acceso.getCursos();
             Curso curso = new Curso(
@@ -218,20 +223,20 @@ public class DialogueCursoController implements Initializable {
                     dateInicio.getValue(), //Fecha de inicio
                     dateFin.getValue(), //Fecha de fin
                     LocalTime.of(spinnerHours.getValue(), spinnerMinutes.getValue()), //Hora
-                    getDias(), //Dias de la semana que se imparte
+                    getDias(), //Dias de la SEMANA que se imparte
                     textAula.getText()); //Aula
             listCursos.add(curso);
             acceso.salvar();
             closeDialogue(event);
-        }
+        } catch (InputMismatchException e) { errorAlert.show(); }
     }
-    
+
     @FXML
     private void closeDialogue(ActionEvent event) {
         Node mynode = (Node) event.getSource();
         mynode.getScene().getWindow().hide();
     }
-    
+
     /**
      * Initializes the controller class.
      */
@@ -241,5 +246,5 @@ public class DialogueCursoController implements Initializable {
         //spinnerMaxAlumnos.getEditor().textProperty().addListener();
         spinnerHours.setValueFactory(new CyclicIntegerSpinnerValueFactory(0, 23));
         spinnerMinutes.setValueFactory(new CyclicIntegerSpinnerValueFactory(0, 59));
-    }    
+    }
 }
